321125b5042be98b6fcc8a9361a82c7b17e68ced
Jason Bartley <83716676+jasonbartley3990@users.noreply.github.com>
2024-11-01 09:01:45 -0400

    Initial Commit

---
+++ MeAmoraNails/MeAmoraNailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeAmoraNailsApp.swift
+//  MeAmoraNails
+//
+//  Created by Jason bartley on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MeAmoraNailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
